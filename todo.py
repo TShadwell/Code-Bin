8df381e5c296c97c517d7df2da4b172433f4a374
Thomas NJ Shadwell <thomas.shadwell@googlemail.com>
2011-10-30 00:21:32 +0100

    Added some better explaination

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
-#Short program to get todo list from taskwarrior
+#(Very) short Python script to construct a todo list (or any kind of list) from Taskwarrior (task).
 from os import popen 
 print(" ".join([x.split(" ")[-1] for x in popen("task next").read().split("\n")[3:-3]]))
