bdeb11e5f594112bf92b857d81dad7681e905f6c
Thomas NJ Shadwell <thomas.shadwell@googlemail.com>
2011-11-13 11:03:19 +0000

    Converted - to spaces

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 #(Very) short Python script to construct a todo list (or any kind of list) from Taskwarrior (task).
 from os import popen 
-print(", ".join([x.split(" ")[-1] for x in popen("task next").read().split("\n")[3:-3]]) + ".")
+print((", ".join([x.split(" ")[-1] for x in popen("task next").read().split("\n")[3:-3]]) + ".").replace("-"," "))
